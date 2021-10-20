ea18dee22024e7ad44d899378ab34a4f8d87d22e
Tom Forbes <tom@tomforb.es>
2021-10-20 16:19:30 +0000

    {"name":"backtrader-doc-zh","version":"2021.10.21.1","file":"backtrader_doc_zh-2021.10.21.1-py3-none-any.whl","path":"backtrader-doc-zh/backtrader_doc_zh-2021.10.21.1-py3-none-any.whl"}

---
+++ backtrader-doc-zh/backtrader_doc_zh-2021.10.21.1-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""backtrader-doc-zh
+https://github.com/apachecn/backtrader-doc-zh"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2021.10.21.1"
