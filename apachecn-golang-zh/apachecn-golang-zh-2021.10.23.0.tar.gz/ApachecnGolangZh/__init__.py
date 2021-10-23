71cc4b9d70502ed5e23f30829c2e61d68e1e33e2
Tom Forbes <tom@tomforb.es>
2021-10-23 15:17:29 +0000

    {"name":"apachecn-golang-zh","version":"2021.10.23.0","file":"apachecn-golang-zh-2021.10.23.0.tar.gz","path":"apachecn-golang-zh/apachecn-golang-zh-2021.10.23.0.tar.gz"}

---
+++ apachecn-golang-zh/apachecn-golang-zh-2021.10.23.0.tar.gz/ApachecnGolangZh/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""apachecn-golang-zh
+https://github.com/apachecn/apachecn-golang-zh"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2021.10.23.0"
