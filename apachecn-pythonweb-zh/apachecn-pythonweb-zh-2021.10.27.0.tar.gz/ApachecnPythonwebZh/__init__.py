ad2ce2486d8adcd5c6b97f1365aff0d375580dcf
Tom Forbes <tom@tomforb.es>
2021-10-27 13:23:09 +0000

    {"name":"apachecn-pythonweb-zh","version":"2021.10.27.0","file":"apachecn-pythonweb-zh-2021.10.27.0.tar.gz","path":"apachecn-pythonweb-zh/apachecn-pythonweb-zh-2021.10.27.0.tar.gz"}

---
+++ apachecn-pythonweb-zh/apachecn-pythonweb-zh-2021.10.27.0.tar.gz/ApachecnPythonwebZh/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""apachecn-pythonweb-zh
+https://github.com/apachecn/apachecn-pythonweb-zh"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2021.10.27.0"
